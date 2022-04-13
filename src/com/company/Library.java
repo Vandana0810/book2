a6869b42ff054bf02612fbbf01dfd2769804a052
Vandana <vandana.srinivasan08@gmail.com>
2022-04-13 16:04:28 +0530

    hey

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Library {
     private String bookname;
+
     public void setBookName(String bookname) {
         this.bookname=bookname;
     }
@@ -13,18 +15,16 @@ public class Library {
 
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
+        int noOfBooks=sc.nextInt();
+        String[] bookNames= new String[noOfBooks];
         Library obj= new Library();
-        String bookname = sc.nextLine();
+        for(int i=0;i<noOfBooks;i++) {
+            bookNames[i]=sc.next();
+        }
+
+        String bookname= Arrays.toString(bookNames);
         obj.setBookName(bookname);
         System.out.println(obj.getBookName());
-    }
+        }
+
 }
-
-
-
-
-
-
-
-
-
